8a1da81bb0d557b6a71d58d223f8c33f5de966a3
Andrew Bernier <a6bernier@gmail.com>
2018-07-29 19:03:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,5 +22,5 @@ function destructivelyAddElementToEndOfArray (array, element) {
 
 function accessElementInArray (array, index) { 
   var chocolateBars = (array, index)
-  console.log(chocolateBars[0])
+  console.log(chocolateBars[3])
 }
